9e53941d0934331c2a8cbc23bd20fd0abd9fc3a8
accxwj <1165172821@qq.com>
2019-05-04 18:08:32 +0800

    5/4

--- controller/UserController.php
+++ controller/UserController.php
@@ -4,11 +4,7 @@
  */
 
 class UserController {
-    //查询推荐课程
-    public  function getRec(){
-        $cou= new CourseModel();
-        $data = $cou->getRec();
-        // var_dump($data);die;
-        // require "./index.php";
-    }
+   
+
+
 }
